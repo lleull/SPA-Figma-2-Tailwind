9f245dec1df3aae13034a9e6544b4ee0c4bb3f9b
lleull <leulmekonnen049@gmail.com>
2024-12-28 19:08:39 +0300

    utilities

---
+++ src/app/utils/landingDatas.ts
@@ -0,0 +1,17 @@
+export const HeaderLists = [{
+    id: 1,
+    name: "About US"
+}, {
+    id: 2,
+    name: "How it Work"
+}, {
+    id: 3,
+    name: "Pricing"
+}, {
+    id: 4,
+    name: "FAq"
+},]
+
+    export landingParagraph = [{
+
+}]
